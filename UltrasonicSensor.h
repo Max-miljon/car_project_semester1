3c86a4782d74ef04aa1fdcef031483ec7dc8d348
cylejaimey <145976760+cylejaimey@users.noreply.github.com>
2024-10-31 10:26:49 +0100

    Add files via upload
    SlavemodeV2 Slavemode nu met aparte header en source files voor functies

---
+++ UltrasonicSensor.h
@@ -0,0 +1,11 @@
+#ifndef ULTRASONIC_SENSOR_H
+#define ULTRASONIC_SENSOR_H
+
+void ultrasonicSetup();
+void sonicCalc();
+void sonicCalc2();
+
+extern float distance;
+extern float distance2;
+
+#endif
